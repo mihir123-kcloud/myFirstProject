70cb8c1ec552414271883f27c56e4b97fecb4627
mihir123-kcloud <mihir.priyadarshi@kcloudtechnologies.com>
2024-07-11 23:44:08 +0530

    changes in details

--- force-app/main/default/lwc/childShowTable/childShowTable.js
+++ force-app/main/default/lwc/childShowTable/childShowTable.js
@@ -3,7 +3,7 @@ export default class ChildShowTable extends LightningElement {
 
     handleClick(){
         let selectEvent = new CustomEvent('select',{
-            detail: {value:'I am from child compoentnt 4 to6',
+            detail: {value:'I am from child compoentnt 4 to 6',
             value2:'this is value 2'}
         });
         this.dispatchEvent(selectEvent);
